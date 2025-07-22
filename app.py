12df4586c1639330e2d654c32f37d5b07e07d3f6
Ansh Mishra <anshmishra3721@gmail.com>
2025-07-22 21:07:10 +0530

    Updated code and added .env (ignored in .gitignore)

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
 
 app= Flask(__name__)
 
+load_dotenv()
+
 #MySQL Configuration 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'users'
-app.secret_key = 'Ansh3721'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.secret_key = os.getenv('SECRET_KEY')
 
 mysql = MySQL(app)
 
